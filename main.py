1ac20a2165bde7955c84ec41dd99a675c5b36b55
Shreyan Ganguly <gangulyofficialshreyan@gmail.com>
2023-05-22 11:18:16 +0530

    Added Slider and minor fix

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import gradio as gr
 import os
 
 
-def image_mod(image):
-    captions = generate_caption().run(image)
-    return captions
+def image_mod(image, N):
+  captions = generate_caption().run(image, N)
+  captions = [caption.replace("\n", "") for caption in captions]
+  captions = '\n'.join([f"- {caption}" for caption in captions])
+  return captions
 
 
 demo = gr.Interface(
     image_mod,
-    inputs=gr.Image(type="pil"),
+    inputs=[gr.Image(type="pil"), gr.Slider(1, 10, value=5, step = 1, label="Caption count", info="Choose between 1 and 10")],
     outputs="text",
 )
 
